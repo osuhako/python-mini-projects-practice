3e373a5ecce3c5d6d6486038896362b7e57d0472
osuhako <alexandr.sugako@gmail.com>
2026-09-13 05:37:56 +0200

    Done

---
+++ Random Password Generator/password-generator.py
@@ -0,0 +1,23 @@
+import random as r
+import string as s
+
+#1
+'''
+chars = []
+for char in s.printable:
+    chars.append(char)
+
+password = ""
+i = 0
+for i in range(13):
+    password += r.choice(chars)
+    i+=1
+'''
+#2
+chars = s.ascii_letters+s.digits+s.punctuation
+def generate_password(length):
+    password = ""
+    for i in range(length):
+        password += r.choice(chars)
+    return password
+
